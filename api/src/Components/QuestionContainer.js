30d416db8ea14d2855061c25aee35a2c39f5656c
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-08-27 19:45:20 -0700

    finishing for the day

--- api/src/Components/QuestionContainer.js
+++ api/src/Components/QuestionContainer.js
@@ -5,7 +5,11 @@ import {
 	fetchCatagorySpecificQuestion,
 } from "../Redux/data/dataActions";
 import { useStyles } from "../Styles/Styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
 import CatagorySelector from "./CatagorySelector";
+import Button from "@material-ui/core/Button";
 
 function QuestionContainer(
 	{ data, fetchUsers, fetchCatagorySpecificQuestion, loading, error  },
@@ -19,10 +23,10 @@ function QuestionContainer(
 	useEffect(() => {
 		fetchUsers();
 	}, []);
-
-
+// const incorrect = [data.incorrect_answers]
+// console.log(incorrect)
 	return loading ? (
-		<h2>Loading</h2>
+		<h2 className={classes.centerText}>Loading</h2>
 	) : error ? (
 		<h2>{error}</h2>
 	) : (
@@ -30,9 +34,28 @@ function QuestionContainer(
 			<h2 className={classes.header}>TRIVIA TIME</h2>
 
 			<CatagorySelector categorySelector={categorySelector} />
-			<div>
-				<p>{data.question}</p>
-			</div>
+			<Grid container className={classes.questionContainer}>
+				<Paper className={classes.question}>
+					<Typography variant="h5" className={classes.question}>
+						Question: {data.question}
+					</Typography>
+				</Paper>
+
+				<Grid container className={classes.buttonContainer2}>
+					<Grid item className={classes.answerHeader}>
+						<Button className={classes.buttons}>{data.correct_answer}</Button>
+						{/* {incorrect.map((incorrect) => */}
+							<Button className={classes.buttons}>{data.incorrect}</Button>
+						{/* )} */}
+						{/* <Button className={classes.buttons}>
+							{data.incorrect_answers[1]}
+						</Button>
+						<Button className={classes.buttons}>
+							{data.incorrect_answers[2]}
+						</Button> */}
+					</Grid>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
--- api/src/Styles/Styles.js
+++ api/src/Styles/Styles.js
@@ -1,6 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles({
+	centerText: {
+		width: "100%",
+		textAlign: "center",
+	},
 	header: {
 		display: "flex",
 		justifyContent: "center",
@@ -10,14 +14,13 @@ export const useStyles = makeStyles({
 		justifyContent: "center",
 	},
 	buttonItem: {
-        display: "flex",
-        flexWrap: "wrap",
+		display: "flex",
+		flexWrap: "wrap",
 		justifyContent: "space-between",
-
 	},
 	buttons: {
-        marginTop: "10px",
-        width: "200px",
+		marginTop: "10px",
+		width: "200px",
 		background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
 		border: 0,
 		borderRadius: 3,
@@ -25,5 +28,18 @@ export const useStyles = makeStyles({
 		color: "white",
 		height: 48,
 		padding: "0 30px",
+	},
+	questionContainer: {
+		marginTop: "125px",
+		display: "flex",
+		justifyContent: "center",
+	},
+	question: {
+		BoxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+	},
+	buttonContainer2: {
+        marginTop: "75px",
+		display: "flex",
+		justifyContent: "center",
 	},
 });
